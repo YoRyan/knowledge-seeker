caf62e0d989e0cb5dca18c7791ede00fc28f29cd
Ryan Young <rayoung@utexas.edu>
2018-05-26 21:48:02 -0500

    Lower the default maximum webm length for performance reasons

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ SUBTITLES_FONTSDIR = 'library/'
 SUBTITLES_FONT = 'Herculanum'
 
 MAX_GIF_LENGTH = timedelta(seconds=10)
-MAX_WEBM_LENGTH = timedelta(seconds=20)
+MAX_WEBM_LENGTH = timedelta(seconds=15)
 GIF_VRES = 360
 WEBM_VRES = 480
 HTTP_CACHE_EXPIRES = timedelta(days=30)
