3a2ec2783f8511358bff4973bc6773be387690ed
Ryan Young <rayoung@utexas.edu>
2018-06-03 18:31:09 -0500

    Reduce default HTTP Expires time

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ MAX_GIF_LENGTH = timedelta(seconds=10)
 MAX_WEBM_LENGTH = timedelta(seconds=15)
 GIF_VRES = 360
 WEBM_VRES = 480
-HTTP_CACHE_EXPIRES = timedelta(days=30)
+HTTP_CACHE_EXPIRES = timedelta(days=7)
