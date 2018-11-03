5a9700a5e61e0b72953eba5658e73da351469c69
Ryan Young <ryan.ry.young@gmail.com>
2018-11-03 16:02:12 -0700

    Remove broken animation endpoints.

--- knowledgeseeker/clipper.py
+++ knowledgeseeker/clipper.py
@@ -14,11 +14,6 @@ from PIL import Image, ImageDraw, ImageFont
 from wsgiref.handlers import format_date_time
 
 from knowledgeseeker.database import get_db
-from .utils import (Timecode, match_season_episode, episode_has_subtitles,
-                    parse_timecode, check_timecode_range, set_expires, static_cached)
-from .video import (make_snapshot, make_snapshot_with_subtitles, make_tiny_snapshot,
-                    make_gif, make_gif_with_subtitles,
-                    make_webm, make_webm_with_subtitles)
 
 bp = flask.Blueprint('clipper', __name__)
 
@@ -63,60 +58,6 @@ def snapshot_tiny(season, episode, ms):
     if res is None:
         flask.abort(404)
     return flask.Response(res['jpeg'], mimetype='image/jpeg')
-
-@bp.route('/<season>/<episode>/<start_timecode>/<end_timecode>/gif')
-@match_season_episode
-@parse_timecode('start_timecode')
-@parse_timecode('end_timecode')
-@check_timecode_range('start_timecode', 'end_timecode',
-                      lambda: flask.current_app.config['MAX_GIF_LENGTH'])
-@set_expires
-def gif(season, episode, start_timecode, end_timecode):
-    data = make_gif(episode.video_path, start_timecode, end_timecode,
-                    vres=flask.current_app.config['GIF_VRES'])
-    return flask.Response(data, mimetype='image/gif')
-
-@bp.route('/<season>/<episode>/<start_timecode>/<end_timecode>/gif/sub')
-@match_season_episode
-@episode_has_subtitles
-@parse_timecode('start_timecode')
-@parse_timecode('end_timecode')
-@check_timecode_range('start_timecode', 'end_timecode',
-                      lambda: flask.current_app.config['MAX_GIF_LENGTH'])
-@set_expires
-def gif_with_subtitles(season, episode, start_timecode, end_timecode):
-    data = call_with_fonts(make_gif_with_subtitles,
-                           episode.video_path,
-                           episode.subtitles_path, start_timecode, end_timecode,
-                           vres=flask.current_app.config['GIF_VRES'])
-    return flask.Response(data, mimetype='image/gif')
-
-@bp.route('/<season>/<episode>/<start_timecode>/<end_timecode>/webm')
-@match_season_episode
-@parse_timecode('start_timecode')
-@parse_timecode('end_timecode')
-@check_timecode_range('start_timecode', 'end_timecode',
-                      lambda: flask.current_app.config['MAX_WEBM_LENGTH'])
-@set_expires
-def webm(season, episode, start_timecode, end_timecode):
-    data = make_webm(episode.video_path, start_timecode, end_timecode,
-                     vres=flask.current_app.config['WEBM_VRES'])
-    return flask.Response(data, mimetype='video/webm')
-
-@bp.route('/<season>/<episode>/<start_timecode>/<end_timecode>/webm/sub')
-@match_season_episode
-@episode_has_subtitles
-@parse_timecode('start_timecode')
-@parse_timecode('end_timecode')
-@check_timecode_range('start_timecode', 'end_timecode',
-                      lambda: flask.current_app.config['MAX_WEBM_LENGTH'])
-@set_expires
-def webm_with_subtitles(season, episode, start_timecode, end_timecode):
-    data = call_with_fonts(make_webm_with_subtitles,
-                           episode.video_path,
-                           episode.subtitles_path, start_timecode, end_timecode,
-                           vres=flask.current_app.config['WEBM_VRES'])
-    return flask.Response(data, mimetype='video/webm')
 
 
 def drawtext(image, top_text, bottom_text):
